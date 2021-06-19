c94e48b00e5e4b01186fcd546f052251df60b4ac
josiela <joseffa.steuernagel@haw-hamburg.de>
2021-06-19 18:10:23 +0200

    Verification logic with new PinInput-Component

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,24 +1,88 @@
 import { VFC } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 
+import { ReactComponent as OKFLogo } from '../assets/images/okf-logo.svg';
+import { PinInput } from '../components/PinInput';
 import { useAuthStore } from '../stores/auth';
 
 export type AuthPageProps = {};
 
+// Pin componente einbinden
+// Function für das submitten aus der Pin-Komponente
+//    --> Diese muss an die Komponente gepassed werden
+//  Was soll in dieser Funktion passieren?
+// Eigentlich erstmal das, was momentan noch über den Button-Click passiert + redirect zu richtiger Seite
+
+const AuthPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: none;
+  color: #ffffff;
+  background: #fff;
+`;
+
+const LogoStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.4rem;
+
+  width: 80vw;
+
+  .logo {
+    width: 8rem;
+  }
+`;
+
+const IntroBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+
+  width: 80vw;
+
+  text-align: center;
+  font-size: 1rem;
+  font-family: ${({ theme }) => theme.fonts.body};
+  color: #000;
+
+  > h2 {
+    font-size: 1.7rem;
+  }
+`;
+
 export const AuthPage: VFC<AuthPageProps> = (): JSX.Element => {
-  const { authenticate, isAuthenticated } = useAuthStore((s) => ({
+  // For redirects
+  const history = useHistory();
+
+  const { authenticate } = useAuthStore((s) => ({
     authenticate: s.authenticate,
     isAuthenticated: s.isAuthenticated,
   }));
 
+  // Compare Pin and redirect if loggedIn
+  const addPin = (pin: string) => {
+    authenticate(pin);
+    history.push('/app');
+  };
+
   return (
-    <div>
-      AuthPage
-      <Link to="/app">Open App</Link>
-      <button type="button" onClick={() => authenticate('dev')}>
-        Authenticate
-      </button>
-      {isAuthenticated ? 'auth ok' : 'auth not ok'}
-    </div>
+    <AuthPageWrapper>
+      <LogoStack>
+        <OKFLogo className="logo" />
+      </LogoStack>
+      <IntroBody>
+        <h2>Verification</h2>
+        Please enter your pin
+      </IntroBody>
+      <PinInput btnText="Verify" onVerify={addPin} />
+    </AuthPageWrapper>
   );
 };
