c198003bb286d73eedf7c5570e65511785b0db7a
finalforce8 <81227341+finalforce8@users.noreply.github.com>
2021-06-27 19:08:25 +0200

    noch 2 tests

---
+++ src/pages/AuthPage.test.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Theme } from '../utils/Theme';
+import { AuthPage } from './AuthPage';
+
+describe('<AuthPage>', () => {
+  it('renders without errors', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Theme>
+        <BrowserRouter>
+          <AuthPage />
+        </BrowserRouter>
+      </Theme>,
+      div
+    );
+  });
+});
